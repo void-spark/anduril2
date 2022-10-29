50637a19f06cf7cf84469dbc32a8727b37a57e32
void-spark <81029971+void-spark@users.noreply.github.com>
2022-10-29 14:14:58 +0200

    Build latest avr-libc from source for Arch idf. We can build everything on arch now, but still a lot of warnings, and results are still a bit (20-300 bytes) bigger, so several fail. Allow sudo in both idfs

--- docker_arch/idf.sh
+++ docker_arch/idf.sh
@@ -9,9 +9,9 @@ USER_GROUP=$(id -n -g)
 INIT_FILE=/home/$USER_NAME/.profile
 
 docker run -it --rm -v $ROOT_PATH:$ROOT_PATH -w $ROOT_PATH anduril2_arch /bin/bash -c "\
-groupadd -g $USER_GID `id -n -g`;\
-useradd -u $USER_UID -g $USER_GID $USER_NAME;\
-echo 'export ATTINY_DFP=/opt/dfp' >> $INIT_FILE;\
-echo 'cd $ROOT_PATH/ToyKeeper/spaghetti-monster/anduril/' >> $INIT_FILE;\
-echo 'echo Hello!' >> $INIT_FILE;\
-su -s /bin/bash --login $USER_NAME"
+groupadd -g $USER_GID `id -n -g` \
+&& useradd -u $USER_UID -g $USER_GID $USER_NAME \
+&& echo '$USER_NAME ALL=(ALL:ALL) NOPASSWD:ALL' > /etc/sudoers.d/$USER_NAME \
+&& echo 'cd $ROOT_PATH/ToyKeeper/spaghetti-monster/anduril/' >> $INIT_FILE \
+&& echo 'echo Hello!' >> $INIT_FILE \
+&& su -s /bin/bash --login $USER_NAME"
